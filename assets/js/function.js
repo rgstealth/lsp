57ce985db49a8a4abd32d87854b6a024f11007f2
Rizkie Giftiandyoko <rizkie.giftiandyoko@gmail.com>
2021-10-27 11:46:14 +0700

    Add files via upload

---
+++ assets/js/function.js
@@ -0,0 +1,52 @@
+/*variabel*/
+let str1 = "Coky"
+let str2 = "String2"
+
+/*constanta*/
+const country = "indonesia"
+
+//alert(str1 + " " + str2)
+
+
+/*conditional*/
+let nilai = 30
+let a = 60;
+let b = 80;
+let pesan;
+
+if (nilai <= a ) {
+    pesan = "Nilai = " + nilai + " dalam Huruf C"
+} else {
+    if (nilai > a && nilai < b) {
+        pesan = "Nilai = " + nilai + " dalam Huruf B"
+    } else {
+        pesan = "Nilai = " + nilai + " dalam Huruf A"
+    }
+}
+
+alert(pesan)
+
+/*looping*/
+for (let i=0;i<=10;i++){
+   // console.log("loop 1 : " + i)
+}
+
+let j = 0;
+while (j<=10) {
+   // console.log("loop 2 : " + j)
+    j++;
+}
+
+/* Function */
+function greeting() {
+    if (str1) {
+    //    console.log("Username teridentifikasi " + str1)
+    } else {
+    //    console.log("Username tidak teridentifikasi")
+    }
+}
+
+//greeting();
+
+
+/* Struktur Data */
